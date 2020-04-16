fe17042e0e8225e53e5b839909bbf2620e080657
Andhika Putra Setianto <andhika312@gmail.com>
2020-04-16 21:27:25 +0700

    DataHelper

---
+++ app/src/main/java/id/ac/umy/exercisesqlite/DataHelper.java
@@ -0,0 +1,26 @@
+package id.ac.umy.exercisesqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DataHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "Contact.db";
+    private static final int DATABASE_VERTSION = 1;
+    public DataHelper(Context context){
+        super(context, DATABASE_NAME,null,DATABASE_VERTSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        String sql = "create table biodata(nama text null, no integer null, email text null, alamat text null);";
+        Log.d("Data","onCreate: "+sql);
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2){
+
+    }
+
+}
